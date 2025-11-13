fd2a672cc06caa57c3ae4b63c9fdaba196e1c500
mohab ali <mohabzalat20@gmail.com>
2025-11-13 18:01:23 +0200

    feat: adding products api and hook

---
+++ client/src/api/products.ts
@@ -0,0 +1,44 @@
+import type Filters from "@/interfaces/products.filters.interface";
+import client from "./apiClient";
+import type Product from "@/interfaces/product.interface";
+
+export const ProductsApi = {
+  getAll: async (page: number, limit: number, filters: Filters) => {
+    const params = new URLSearchParams({
+      page: page.toString(),
+      limit: limit.toString(),
+      sortBy: filters.sortBy,
+      sortOrder: filters.sortOrder,
+    });
+
+    if (filters.search) params.append("search", filters.search);
+    if (filters.type) params.append("type", filters.type);
+    if (filters.categoryId) params.append("categoryId", filters.categoryId);
+    if (Object.keys(filters.attributeFilters).length > 0) {
+      params.append("attributes", JSON.stringify(filters.attributeFilters));
+    }
+
+    const response = await client.get(`/products?${params.toString()}`);
+    return response.data;
+  },
+
+  getById: async (id: string) => {
+    const response = await client.get(`/products/${id}`);
+    return response.data;
+  },
+
+  create: async (payload: Partial<Product>) => {
+    const response = await client.post("/products", payload);
+    return response.data;
+  },
+
+  update: async (id: string, payload: Partial<Product>) => {
+    const response = await client.put(`/products/${id}`, payload);
+    return response.data;
+  },
+
+  delete: async (id: string): Promise<void> => {
+    const response = await client.delete(`/products/${id}`);
+    return response.data;
+  },
+};
---
+++ client/src/hooks/useProducts.ts
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import { ProductsApi } from "@/api/products";
+import type Product from "@/interfaces/product.interface";
+import type Filters from "@/interfaces/products.filters.interface";
+
+export function useProducts(page: number, limit: number, filters: Filters) {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<Error | null>(null);
+
+  const fetchProducts = async () => {
+    setLoading(true);
+    try {
+      const response = await ProductsApi.getAll(page, limit, filters);
+      setProducts(response.data);
+    } catch (err: unknown) {
+      const error = err as Error;
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, [page, limit, filters]);
+
+  return [products, loading, error] as const;
+}
