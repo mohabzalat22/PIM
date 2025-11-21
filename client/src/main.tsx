2398d68e8a68e53fd0784443283995431a8a728e
mohab ali <mohabzalat20@gmail.com>
2025-11-22 00:59:42 +0200

    fix : fixing strict mode re-rendering

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,29 +1,23 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import React from "react";
 import { ThemeProvider } from "@/components/theme-provider";
-import { ClerkProvider } from '@clerk/clerk-react'
+import { ClerkProvider } from "@clerk/clerk-react";
 
 // Import your Publishable Key
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 if (!PUBLISHABLE_KEY) {
-  throw new Error('Add your Clerk Publishable Key to the .env file')
+  throw new Error("Add your Clerk Publishable Key to the .env file");
 }
-
+// TODO : Wrap StrictMode only once
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <React.StrictMode>
-      <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
-        <BrowserRouter>
-         <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-          <App />
-         </ClerkProvider>
-        </BrowserRouter>
-      </ThemeProvider>
-    </React.StrictMode>
-  </StrictMode>
+  <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+    <BrowserRouter>
+      <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+        <App />
+      </ClerkProvider>
+    </BrowserRouter>
+  </ThemeProvider>
 );
